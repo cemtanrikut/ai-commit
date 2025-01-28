6c5d2fc202a06fca5c7dee58849cb3e6005219cc
Cem Tanrikut <cemtanrikut@gmail.com>
2025-01-28 06:15:06 +0100

    check diff message

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
-package aicommit
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"main.go/git"
+)
 
 func main() {
+	// Get git diff result
+	diff, err := git.GetDiff()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 
+	// Check diff empty or not
+	if diff == "" {
+		fmt.Println("No staged changes found. Please stage your changes with 'git add'")
+		os.Exit(1)
+	}
+
+	fmt.Println(diff)
 }
