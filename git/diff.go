f23f9b0e8ba9f92a6756b5c57b3336cdd97d787e
Cem Tanrikut <cemtanrikut@gmail.com>
2025-01-28 07:38:35 +0100

    added update message choose for user

--- git/diff.go
+++ git/diff.go
@@ -2,7 +2,9 @@ package git
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 // GetDiff, gets diff from staged changes
@@ -14,4 +16,54 @@ func GetDiff() (string, error) {
 	}
 
 	return string(output), nil
+}
+
+// Runs git commit script
+func RunGitCommit(message string) error {
+	cmd := exec.Command("git", "commit", "-m", message)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// User can edit message with an editor
+func EditCommitMessage(initalMessage string) (string, error) {
+	// Generate temp file
+	tempFile, err := os.CreateTemp("", "commit-message-*.txt")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temp file: %v", err)
+	}
+
+	// Delete temp file after process complete
+	defer os.Remove(tempFile.Name())
+
+	// Write initial message to file
+	if _, err := tempFile.WriteString(initalMessage); err != nil {
+		return "", fmt.Errorf("failed to write to temp file: %v", err)
+	}
+	tempFile.Close()
+
+	// Get user's editor
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		// If editor doesn't choose select VIM
+		editor = "vim"
+	}
+
+	// Open editor
+	cmd := exec.Command(editor, tempFile.Name())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("failed to open editor: %v", err)
+	}
+
+	// Read edited message from file
+	editedMessageBytes, err := os.ReadFile(tempFile.Name())
+	if err != nil {
+		return "", fmt.Errorf("failed to read edited message: %v", err)
+	}
+
+	return strings.TrimSpace(string(editedMessageBytes)), nil
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"main.go/api"
 	"main.go/git"
@@ -34,5 +36,37 @@ func main() {
 	// Show the generated commit message
 	fmt.Println("Generated Commit Message:")
 	fmt.Println(commitMessage)
+
+	// Approvement check
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("\nDo you approve this commit message and commit changes? (y/n)")
+	answer, _ := reader.ReadString('\n')
+	answer = strings.TrimSpace(strings.ToLower(answer))
+
+	if answer == "y" || answer == "yes" {
+		// Run commit process
+		err := git.RunGitCommit(commitMessage)
+		if err != nil {
+			fmt.Println("Failed to commit: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Changes committed successfully!")
+	} else {
+		// Open an editor for edit commit message
+		editedMessage, err := git.EditCommitMessage(commitMessage)
+		if err != nil {
+			fmt.Printf("Error editing message: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Commit when user complete edit
+		err = git.RunGitCommit(editedMessage)
+		if err != nil {
+			fmt.Printf("Failed to commit: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Changes committed successfully!")
+	}
 
 }
